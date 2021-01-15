b186b23637dd1278571066f8e9dd73e029982424
Quang Huynh <quang@bluematterscience.com>
2021-01-14 16:28:55 -0800

    Add verbosity arg to r4ss fn

--- 03_run_ss.R
+++ 03_run_ss.R
@@ -14,26 +14,26 @@ run_ss <- function(dir, covar = TRUE, output = FALSE) {
   return(invisible(out))
 }
 
-plot_ss <- function(dir) {
+plot_ss <- function(dir, verbose = TRUE) {
   curdir <- getwd()
   on.exit(setwd(curdir))
   
   setwd(file.path(curdir, "SS", dir))
-  replist <- r4ss::SS_output(getwd())
+  replist <- r4ss::SS_output(getwd(), verbose = verbose)
   replist$btarg <- replist$minbthresh <- -1
   
-  r4ss::SS_plots(replist)
+  r4ss::SS_plots(replist, verbose = verbose)
   return(invisible(replist))
 }
 
 
-dir <- "01_base"
+dir <- "01_base_offset"
 dir <- "02_agesel"
 dir <- "05_maxF_high"
 dir <- "03_area"
 run_ss(dir)
 
-replist <- plot_ss(dir)
+replist <- plot_ss(dir, FALSE)
 replist <- r4ss::SS_output(file.path(getwd(), "SS", dir))
 
 r4ss::SSplotMovementRates(replist)
@@ -42,3 +42,10 @@ replist$timeseries %>% View()
 replist$derived_quants %>% View()
 replist$estimated_non_dev_parameters
 replist$movement
+
+
+## ENSURE catch multiplier works
+plot(replist$timeseries$Yr, replist$timeseries$`obs_cat:_3`, typ = 'o')
+lines(replist$timeseries$Yr, replist$timeseries$`dead(B):_3`, typ = 'o', col = 'red')
+
+replist$timeseries$`dead(B):_3`/replist$timeseries$`obs_cat:_3`
